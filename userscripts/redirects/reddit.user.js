e9535cfe4acd5a4977ff6c6ceea4e9e22256cab3
Alice Nyaa <62451415+Minnowo@users.noreply.github.com>
2024-01-04 00:07:59 -0500

    Update reddit.user.js

--- userscripts/redirects/reddit.user.js
+++ userscripts/redirects/reddit.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name        Libreddit
 // @namespace   Violentmonkey Scripts
-// @match       *://*.reddit.com/*
+// @match       *://*reddit.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      -
 // @description Redirect reddit to a libreddit instance
 // ==/UserScript==
 
 // https://github.com/libreddit/libreddit-instances/blob/master/instances.md
+const INSTANCE = "reddit.invak.id";
 
 url = location.href;
 url = url.replace(/\bwww\.\b/, "");
-url = url.replace("reddit.com", "reddit.invak.id");
+url = url.replace("old.reddit.com", INSTANCE);
+url = url.replace("reddit.com", INSTANCE);
 
 location.href = url;
